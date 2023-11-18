39af034d1b01f7c0ee4bd54d505bfd44675196eb
AvaterClasher <avater.clasher@gmail.com>
2023-11-18 22:57:49 +0530

    Release v0.1.0

--- src/components/NavFiles.tsx
+++ src/components/NavFiles.tsx
@@ -33,7 +33,7 @@ export default function NavFiles({files, visible}: Props) {
 
       return <div onClick={(ev) => onShow(ev, file)}
         key={file.id}
-        className={`soure-item ${isSelected ? 'source-item-active' : ''} flex items-center gap-2 px-2 py-0.5 text-gray-500 hover:text-gray-400 cursor-pointer`}
+        className={`source-item ${isSelected ? 'source-item-active' : ''} flex items-center gap-2 px-2 py-0.5 text-gray-500 hover:text-gray-400 cursor-pointer`}
       >
         <FileIcon name={file.name} />
         <span>{file.name}</span>
